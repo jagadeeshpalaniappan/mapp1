a741c4bfd1faaf12f62e5d13f2d9d3b6db1b157e
Jagadeesh Palaniappan <jagadeeshthegeek@gmail.com>
2017-04-06 23:45:03 -0700

    RESTRICT::  //    -Request is Called By Micro App Framework  //    -User is Authenticated

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -7,27 +7,62 @@ var validator = require('validator'),
 /**
  * Render the main application page
  */
-exports.renderIndex = function (req, res) {
-  var safeUserObject = null;
-  if (req.user) {
-    safeUserObject = {
-      displayName: validator.escape(req.user.displayName),
-      provider: validator.escape(req.user.provider),
-      username: validator.escape(req.user.username),
-      created: req.user.created.toString(),
-      roles: req.user.roles,
-      profileImageURL: req.user.profileImageURL,
-      email: validator.escape(req.user.email),
-      lastName: validator.escape(req.user.lastName),
-      firstName: validator.escape(req.user.firstName),
-      additionalProvidersData: req.user.additionalProvidersData
-    };
-  }
+
+var _renderIndex = function (req, res) {
+
+  // User is Authenticated ::
+
+  var safeUserObject = {
+    displayName: validator.escape(req.user.displayName),
+    provider: validator.escape(req.user.provider),
+    username: validator.escape(req.user.username),
+    created: req.user.created.toString(),
+    roles: req.user.roles,
+    profileImageURL: req.user.profileImageURL,
+    email: validator.escape(req.user.email),
+    lastName: validator.escape(req.user.lastName),
+    firstName: validator.escape(req.user.firstName),
+    additionalProvidersData: req.user.additionalProvidersData
+  };
 
   res.render('modules/core/server/views/index', {
     user: JSON.stringify(safeUserObject),
     sharedConfig: JSON.stringify(config.shared)
   });
+
+
+};
+
+
+exports.renderIndex = function (req, res) {
+
+  var magicKey = req.get('source-system');
+  var isCalledByMicroAppFramework = magicKey && magicKey === 'SUPERMICROAPPFRAMEWORK' ? true : false;
+
+  if (isCalledByMicroAppFramework) {
+
+    if (req.user) {
+
+      // SUCESS::
+      //    -Request is Called By Micro App Framework
+      //    -User is Authenticated
+      _renderIndex(req, res);
+
+    } else {
+
+      // User is NOT Authenticated ::
+      res.status(401).json({message: 'User is not authenticated, Please login to access this Micro App'});
+
+    }
+
+  } else {
+
+    // Request is NOT Called By MicroAppFramework ::
+    res.status(400).json({message: 'Request is not Called By MicroAppFramework, Please access this Micro App via MicroAppFramework'});
+
+  }
+
+
 };
 
 /**
